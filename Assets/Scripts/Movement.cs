e9c46b9a966b2c6095854a75669e36f63f1f492c
Soban Syeed <Soban.syeed@outlook.com>
2025-02-08 22:06:35 +0530

    Add Abstruction to movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength = 1000f;
     Rigidbody rb;
 
@@ -15,13 +16,26 @@ public class Movement : MonoBehaviour
     void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+
+    void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log(rotationInput);
     }
 }
